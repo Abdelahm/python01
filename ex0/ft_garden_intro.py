341acb5d1741b1564112e599267801dc8bc36859
Abdelrahman Ahmed <abdelahm@c3r3s1.42wolfsburg.de>
2026-05-05 20:25:35 +0200

    2nd day

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    name = 'Rose'
+    Height = 25
+    Age = 30
+    print(f"plant: {name}")
+    print(f"Height: {Height}cm")
+    print(f"Age: {Age} days")
+    print("=== End of Program ===")
---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,6 @@
+class plant:
+    def show(self) -> None:
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+if __name__ == "__main__":
+    
